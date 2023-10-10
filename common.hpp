03740e5e100b1592453c838c9bdceb632050a2d3
Amiko Malania <amikomalania@gmail.com>
2023-10-10 21:02:21 +0400

    In the middle of implementing check_huge_page function

--- common.hpp
+++ common.hpp
@@ -148,7 +148,130 @@ static void parse_options(int argc, char** argv, Options& options)
     while (true)
     {
         // TODO : Implement
+		int option_index;
+		int c = getopt_long(argc, argv, "", long_options, &option_index);
+		if (c == -1)
+		{
+			break;
+		} 
+		else if (c == '?')
+		{
+			optind--;
+			fprintf(stderr, "bad usage, non-operation arguments starting from:\n ");
+			while (optind < argc)
+			{
+				fprintf(stderr, "%s ", argv[optind++]);
+			}
+			fprintf(stderr, "\n");
+			exit(EXIT_FAILURE);
+		}
+		else if(c == 0)
+		{
+			const char* option = long_option[option_index].name;
+			options.busy_loop = options.busy_loop || (strcmp("busy_loop", option) == 0);
+			options.poll = options.poll || (strcmp("poll", option) == 0);
+			options.huge_page = options.huge_page || (strcmp("huge_page", option) == 0);
+			options.check_huge_page = options.check_huge_page || (strcmp("check_huge_page", option) == 0);
+			options.write_with_vmsplice = options.write_with_vmsplice || (strcmp("write_with_splice", option) == 0);
+			options.read_with_splice = options.read_with_splice || (strcmp("read_with_splice", option) == 0);
+			verbose = verbose || (strcmp("verbose", option) == 0);
+			options.gift = options.gift || (strcmp("gift", option) == 0);
+			options.lock_memory = options.lock_memory || (strcmp("lock_memory", option) == 0);
+			options.dont_touch_pages = options.dont_touch_pages || (strcmp("dont_touch_pages", option) == 0);
+			options.same_buffer = options.same_buffer || (strcmp("same_buffer", option) == 0);
+			options.csv = options.csv || (strcmp("csv", option) == 0);
+
+			if (strcmp("buf_size", option) == 0)
+			{
+				options.buf_size = read_size_str(optarg);			
+			}
+			if (strcmp("bytes_to_pipe", option) == 0)
+			{
+				options.bytes_to_pipe = read_size_str(optarg);
+			}
+		}
+		else
+		{
+			fail("getopt returned character cpde 0%o\n", c);
+		}
     }
+
+	if (options.dont_touch_pages && options.check_huge_pages)
+	{
+    fail("--dont_touch_pages and --check_huge_page are incompatible -- we can't the huge pages if we don't fault them in first.\n");
+
+	}
+
+	const auto bool_str = [](const bool b) {
+		if (b) {return "true";}
+		else {retur "false";}
+	};
+
+	log("busy_loop\t\t%s\n", bool_str(options.busy_loop));
+	log("poll\t\t\t%s\n", bool_str(options.poll));
+	log("huge_page\t\t%s\n", bool_str(options.huge_page));
+	log("check_huge_page\t\t%s\n", bool_str(options.check_huge_page));
+	log("buf_size\t\t%zu\n", options.buf_size);
+	log("write_with_vmsplice\t%s\n", bool_str(options.write_with_vmsplice));
+	log("read_with_splice\t%s\n", bool_str(options.read_with_splice));
+	log("gift\t\t\t%s\n", bool_str(options.gift));
+	log("lock_memory\t\t%s\n", bool_str(options.lock_memory));
+	log("dont_touch_pages\t%s\n", bool_str(options.dont_touch_pages));
+	log("same_buffer\t\t%s\n", bool_str(options.same_buffer));
+	log("csv\t\t\t%s\n", bool_str(options.csv));
+	log("bytes_to_pipe\t\t%zu\n", options.bytes_to_pipe);
+	log("pipe_size\t\t%zu\n", options.pipe_size);
+	log("\n");
 }
+
+#define PAGEMAP_PRESENT(ent) (((ent) & (1ull << 63)) != 0)
+#define PAGEMAP_PEN ((ent) & ((1ull << 55) - 1))
+
+static void check_huge_page(void *ptr)
+{
+	if (prctl(PR_SET_DUMPABLE, 1, 0, 0) < 0)
+	{
+		fail("could not set the process as dumpable: %s", strerror(errno));
+	}
+
+	int pagemap_fd = open("proc/self/pagemap", O_RDONLY);
+	if (pagemap_fd < 0)
+	{
+		fail("could not open /proc/self/pagemap: %s", strerror(errno));
+	}
+
+	int kpageflags_fd = open("/proc/kpageflags: %s", strerror(errno));
+	if (kpageflags_fd < 0)
+	{
+		fail("could not open /proc/self/kpageflags: %s", strerror(errno));
+	}
+
+	// each entry is 8 bytes long, so to get the offset in pagemap we need to
+	// ptr / PAGE_SIZE * 8, or equivalently ptr >> (PAGE_SHIFT - 3)
+	uint64_t ent;
+	if (pread(pagemap_fd, &ent, sizeof(ent), ((uintptr_t) ptr) >> (PAGE_SHIFT - 3)) != sizeof(ent))
+	{
+		failt("could not read from pagemap\n");
+	}
+
+	// TODO : Finish implementing
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif 
